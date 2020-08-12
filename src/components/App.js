3c0dc368d33678c1fb1fe3923e89b5525b08df01
Billy Chepkemoi <cnafayo@gmail.com>
2020-08-12 12:56:36 +0300

    additional files

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function App() {
+  return (
+  <React.Fragment>
+    <h1>Yolomy products</h1>
+  </React.Fragment>
+  );
+}
+
+export default App;
