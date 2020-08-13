066f1589b269196e4c3ec2aa9fa38e8f78c66482
Billy Chepkemoi <cnafayo@gmail.com>
2020-08-13 05:51:25 +0300

    additional files header and Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../App.css';
-
+import Header from './Header'
 function App() {
   return (
   <React.Fragment>
-    <h1>Yolomy products</h1>
+<div>
+  <Header/>
+</div>
   </React.Fragment>
   );
 }
